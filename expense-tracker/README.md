7fa128af83d5482cb3bceef1f82020145b8c9f8b
Zgurskyi <bzgurskyi@kiev.luxoft.com>
2023-11-22 22:41:56 +0200

    Persisting Redux store

--- expense-tracker/README.md
+++ expense-tracker/README.md
@@ -9,3 +9,6 @@ npm start
 
 to install Redux:
 $ npm i @reduxjs/toolkit@1.8.6 react-redux@8.0.4
+
+$ npm i redux-persist
+
--- expense-tracker/src/store/index.js
+++ expense-tracker/src/store/index.js
@@ -1,10 +1,44 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { expenseSlice } from "./expense/expense-slice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const store = configureStore({
-  reducer: {
-    EXPENSE: expenseSlice.reducer,
-  },
+// const store = configureStore({
+//   reducer: {
+//     EXPENSE: expenseSlice.reducer,
+//   },
+// });
+
+const rootReducer = combineReducers({
+  EXPENSE: expenseSlice.reducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ['EXPENSE'], //optional, only these reducers will be persisted
+  blacklist: [''] //optional, reducers which will not be persisted
+};
+
+const persistedReducers = persistReducer(persistConfig, rootReducer);
+const store = configureStore({
+  reducer: persistedReducers,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export const persistor = persistStore(store);
 
 export { store };
